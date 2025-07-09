f0c0dcc13d3e40f6715946ee27821ede5b3bac92
FarazMehraien <farazmehraien95@gmail.com>
2025-07-09 17:16:11 +0330

    seperate containers

---
+++ src/container/PhoneNumberStep.tsx
@@ -0,0 +1,36 @@
+import Button from '../components/Button'
+import PhoneInput from '../components/MobileInput'
+
+type Props = {
+  phoneNumber: string
+  handlePhoneChange: (phoneNumber: string) => void
+  submitPhone: () => void
+  loading?: boolean
+}
+
+const PhoneNumberStep = ({
+  phoneNumber,
+  handlePhoneChange,
+  submitPhone,
+  loading
+}: Props) => {
+  return (
+    <div>
+      <p className='text-lg font-semibold text-gray-800 mb-2'>
+        شماره موبایل خود را وارد نمایید.
+      </p>
+      <p className='text-xs text-gray-500 mb-6'>کد تایید برای شما ارسال خواهد شد.</p>
+      <PhoneInput
+        label='تلفن همراه'
+        placeholder='XXX - XXX - XXXX'
+        value={phoneNumber}
+        onChange={handlePhoneChange}
+      />
+      <Button loading={loading} classNames='mt-7' onClick={submitPhone}>
+        ادامه
+      </Button>
+    </div>
+  )
+}
+
+export default PhoneNumberStep
